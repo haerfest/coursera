547b8eb4204f2828dd14dc3f2eb3780978d4ddc4
haerfest <wouter.hobers+github@gmail.com>
2017-10-12 00:44:45 +0200

    Correct PSNR calculation.

--- Fundamentals of Digital Image and Video Processing/Week02/homework2.m
+++ Fundamentals of Digital Image and Video Processing/Week02/homework2.m
@@ -1,26 +1,21 @@
-original = imread('digital-images-week2_quizzes-lena.gif');
-as_double = im2double(original);
+original = im2double(imread('digital-images-week2_quizzes-lena.gif'));
 
-lpf3x3 = [
-    1/9, 1/9, 1/9;
-    1/9, 1/9, 1/9;
-    1/9, 1/9, 1/9
-];
+lpf3x3 = ones(3, 3) .* 1/9;
+lpf5x5 = ones(5, 5) .* 1/25;
 
-lpf5x5 = [
-    1/25, 1/25, 1/25, 1/25, 1/25;
-    1/25, 1/25, 1/25, 1/25, 1/25;
-    1/25, 1/25, 1/25, 1/25, 1/25;
-    1/25, 1/25, 1/25, 1/25, 1/25;
-    1/25, 1/25, 1/25, 1/25, 1/25
-];
+filtered3x3 = imfilter(original, lpf3x3, 'replicate');
+filtered5x5 = imfilter(original, lpf5x5, 'replicate');
 
-filtered3x3 = imfilter(as_double, lpf3x3, 'replicate');
-filtered5x5 = imfilter(as_double, lpf5x5, 'replicate');
+%
+% Note that max_l = 255 for grayscale 8bpp images, but
+% 1.0 for images converted to double.
+%
+max_l = 1.0^2;
 
-max_l = 255^2;
-mse3x3  = sum(sum((filtered3x3 - as_double) .^ 2)) / (256^2);
+mse3x3  = sum(sum((filtered3x3 - original) .^ 2)) / (256^2);
 psnr3x3 = 10 * log10(max_l / mse3x3);
 
-mse5x5  = sum(sum((filtered5x5 - as_double) .^ 2)) / (256^2);
+mse5x5  = sum(sum((filtered5x5 - original) .^ 2)) / (256^2);
 psnr5x5 = 10 * log10(max_l / mse5x5);
+
+fprintf("PSNR 3x3=%.2f, 5x5=%.2f", psnr3x3, psnr5x5);
